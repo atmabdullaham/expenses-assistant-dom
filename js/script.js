016fcb3d23a146940ce9bdf35a1a4fc4c89d98fd
atmabdullaham <atmabdullaham@gmail.com>
2024-12-28 20:45:38 +0600

    add and remove the form

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.getElementById("calculate").addEventListener("click", function (event) 
  const software = parseFloat(document.getElementById("software").value)
  const courses = parseFloat(document.getElementById("courses").value)
  const internet = parseFloat(document.getElementById("internet").value)
-
  document.getElementById("results").classList.remove("hidden");
  const totalExpenses = software + courses + internet;
  document.getElementById("total-expenses").innerText = totalExpenses;
@@ -21,15 +20,29 @@ document.getElementById("calculate-savings").addEventListener("click", function 
  const savings = parseFloat(document.getElementById("savings").value)
  document.getElementById("results").classList.remove("hidden");
  const totalExpenses = software + courses + internet;
-
  const balance = income - totalExpenses;
-
  const percentage = savings / 100;
-
  const savingAmount = balance * percentage
  document.getElementById("savings-amount").innerText = savingAmount;
-
  const remainingBalance = balance - savingAmount;
-
  document.getElementById("remaining-balance").innerText = remainingBalance;
+})
+// function for add class
+function addClass(id, className) {
+ document.getElementById(id).classList.add(className)
+}
+
+// function for remove class
+function removeClass(id, className) {
+ document.getElementById(id).classList.remove(className);
+}
+
+
+document.getElementById("history-tab").addEventListener("click", function () {
+ addClass("history-tab", "text-white")
+ addClass("history-tab", "bg-gradient-to-r")
+ addClass("history-tab", "from-blue-500")
+ addClass("history-tab", "to-purple-600")
+
+ removeClass("history-tab", "text-gray-600")
 })
